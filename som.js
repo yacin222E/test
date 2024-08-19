ec08c4ef6d9906d47682ddf7225d815db414fc1d
unknown <jacob220@hotmail.fr>
2024-08-19 15:28:05 +0200

    voici un test node js

---
+++ som.js
@@ -0,0 +1,4 @@
+function summ(a,b){
+      return a*b 
+}
+module.export = summ
---
+++ somm.test.js
@@ -0,0 +1,7 @@
+describe(
+    'test som', ()=>{
+        test('test 2*3 =6', ()=>{
+            expect(summ(2,3)).toBe(6)
+        })
+    }
+)
